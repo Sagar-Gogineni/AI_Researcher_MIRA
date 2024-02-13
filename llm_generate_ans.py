ff26eb2efa79c83514395b8fc0e4158075993850
Sagar <159469135+Sagar-Gogineni@users.noreply.github.com>
2024-02-13 19:18:32 +0100

    Update llm_generate_ans.py

--- llm_generate_ans.py
+++ llm_generate_ans.py
@@ -5,7 +5,7 @@ def generate_answer(query,similar_text,openAI_API_KEY):
 
     os.environ["OPENAI_API_KEY"] = openAI_API_KEY
     llm=ChatOpenAI(model_name="gpt-3.5-turbo-1106",
-            max_tokens=10)
+            max_tokens=500)
 
     llm_ans=llm.invoke(
         "You are an Intelligent AI research assistant. Your task is to generate answers from similar_text to the user query. Here is the user query: "+query+ ". Here is the similar_text: "+ str(similar_text)
